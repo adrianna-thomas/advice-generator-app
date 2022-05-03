14878e11eec8746a9add029eececce181ca94382
Adrianna Thomas <adrianna.v.thomas@gmail.com>
2022-05-02 21:28:53 -0400

    completed project

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // alert('Hi')
+
+
+const adviceId = document.querySelector('#adviceId');
+const adviceBody = document.querySelector('#adviceText');
+const btn = document.querySelector('.btn');
+
+
+
+fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+}).then(adviceData => {
+    const adviceNum = adviceData.slip.id;
+    const advice = adviceData.slip.advice;
+
+    adviceId.textContent = adviceNum;
+    adviceText.innerHTML = `<p>"${advice}"</p>`;
+}).catch(error => {
+    console.log(error);
+});
+
+btn.addEventListener("click", refreshPage)
+
+function refreshPage() {
+    window.location.reload();
+}
